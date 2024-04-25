e70a72b4845a6bae8e5ad3b77c8dff95195597c1
Prince Ranjan <99893511+princeranjan03@users.noreply.github.com>
2024-04-25 13:06:21 +0530

    Update cal.asm

--- cal.asm
+++ cal.asm
@@ -1,171 +1,171 @@
-INCLUDE EMU8086.INC     ; Including Library
+INCLUDE EMU8086.INC     ;Including Library
 
 org 100h
 
-.data       ; Data Segment
+.data       ;Data Segment
 
-msg_intro db '1. Addition', 0dh,0ah, '2. Subtraction', 0dh,0ah, '3. Multiplication', 0dh,0ah, '4. Division', 0dh,0ah, '5. Negation', 0dh,0ah, '6. Square', 0dh,0ah, '7. Cube', 0dh,0ah, '8. Bitwise OR', 0dh,0ah, '9. Bitwise AND', 0dh,0ah, '10. Bitwise XOR', 0dh,0ah, '11. Bitwise NOT', 0dh,0ah, '12. Modulo', 0dh,0ah, '13. Rotate Left', 0dh,0ah, '14. Rotate Right', 0dh,0ah, '0. Exit', '$'
+msg_intro db '  1. Addition', 0dh,0ah, '  2. Subtraction', 0dh,0ah, '  3. Multiplication', 0dh,0ah, '  4. Division', 0dh,0ah, '  5. Negation', 0dh,0ah, '  6. Square', 0dh,0ah, '  7. Cube', 0dh,0ah, '  8. OR', 0dh,0ah, '  9. AND', 0dh,0ah, ' 10. XOR', 0dh,0ah, ' 11. NOT', 0dh,0ah, ' 12. Modulus', 0dh,0ah, ' 13. Rotate Left', 0dh,0ah, ' 14. Rotate Right', 0dh,0ah, '  0. EXIT', '$'
 
-msg_A db 'Result of Addition = $', 0dh,0ah
-msg_S db 'Result of Subtraction = $', 0dh,0ah
-msg_M db 'Result of Multiplication = $', 0dh,0ah
-msg_D db 'Result of Division = $', 0dh,0ah
-msg_N db 'Negative of Number = $', 0dh,0ah
-msg_SQ db 'Square of the Number = $', 0dh,0ah
-msg_CB db 'Cube of the Number = $', 0dh,0ah
-msg_OR db 'Result of Bitwise OR = $', 0dh,0ah
-msg_AND db 'Result of Bitwise AND = $', 0dh,0ah
-msg_XOR db 'Result of Bitwise XOR = $', 0dh,0ah
-msg_NOT db 'Result of Bitwise NOT = $', 0dh,0ah
-msg_MD db 'Remainder of Division = $', 0dh,0ah
-msg_RL db 'Rotate Left Result = $', 0dh,0ah
-msg_RR db 'Rotate Right Result = $', 0dh,0ah
+msg_A db 'The SUM of two Numbers = $', 0dh,0ah
+msg_S db 'The SUBTRACTION of two Numbers = $', 0dh,0ah
+msg_M db 'The MULTIPLICATION of two Numbers = $', 0dh,0ah
+msg_D db 'The DIVISION of two Numbers = $', 0dh,0ah
+msg_N db 'The NEG value of Number = $', 0dh,0ah
+msg_SQ db 'The SQUARE of Number = $', 0dh,0ah
+msg_CB db 'The CUBE of Number = $', 0dh,0ah
+msg_OR db 'The OR operation of two Numbers = $', 0dh,0ah
+msg_AND db 'The AND operation of two Numbers = $', 0dh,0ah
+msg_XOR db 'The XOR operation of two Numbers = $', 0dh,0ah
+msg_NOT db 'The NOT of Number = $', 0dh,0ah
+msg_MD db 'The MODULUS of Two Numbers = $', 0dh,0ah
+msg_RL db 'The Rotate Left of Number = $', 0dh,0ah
+msg_RR db 'The Rotate Right of Number = $', 0dh,0ah
 
-cont db 'Would you like to continue? (1 = Yes, 0 = No): $'
-bye db 'Thank you for using the calculator! See you next time! $'
+cont db 10,13,'Do you want to Use Again? $'
+bye db '            **** Thank You !!!  :) **** $'
 
-val1 dw ?       ; Uninitialized
-val2 dw ?       ; Uninitialized  
+val1 dw ?       ;Uninitialize
+val2 dw ?       ;Uninitialize  
 res dw ?
 agn dw ?
 
-.code       ; Code Segment
+.code       ;Code Segment
 MAIN PROC
     
 MOV AX, @data
 MOV DS, AX
 
-Start:                  ; Start Label
+Start:                  ;Start Label
 
-print '**** Simple Calculator ****'
-printn     ; New Line
+print '             **** CALCULATOR ****    '  
+printn      ;New Line
 
-print 'Choose an option:'
 printn
-MOV AH, 9
+MOV AH,9
 MOV DX, OFFSET msg_intro
-INT 21h              ; Display the list of operations
+INT 21h                                          ; Display the options list
 
 printn
-print 'Your choice: '
-CALL scan_num           ; Read the user's option
+printn
+print 'Select Any Option : '
+CALL scan_num           ; Read the option
+printn
 printn
 
 CMP CX, 0
-JE _Bye                 ; If user chooses 0, jump to exit
+JE _Bye                 ; Jump to exit
 
 CMP CX, 1
-JE Addition             ; Jump to Addition function
+JE Addition             ; Jump to Addition
 
 CMP CX, 2
-JE Subtraction          ; Jump to Subtraction function
+JE Subtraction          ; Jump to Subtraction
 
 CMP CX, 3
-JE Multiplication       ; Jump to Multiplication function
+JE Multiplication       ; Jump to Multiplication
 
 CMP CX, 4
-JE Division             ; Jump to Division function
+JE Division             ; Jump to Division
 
 CMP CX, 5
-JE Negation             ; Jump to Negation function
+JE Negation             ; Jump to Negation
 
 CMP CX, 6
-JE Square               ; Jump to Square function
+JE Square               ; Jump to Square
 
 CMP CX, 7
-JE Cube                 ; Jump to Cube function
+JE Cube                 ; Jump to Cube
 
 CMP CX, 8
-JE _OR                  ; Jump to OR function
+JE _OR                  ; Jump to OR
 
 CMP CX, 9
-JE _AND                 ; Jump to AND function
+JE _AND                 ; Jump to AND
 
 CMP CX, 10
-JE _XOR                 ; Jump to XOR function
+JE _XOR                 ; Jump to XOR
 
 CMP CX, 11
-JE _NOT                 ; Jump to NOT function
+JE _NOT                 ; Jump to NOT
 
 CMP CX, 12
-JE Modulus              ; Jump to Modulo function
+JE Modulus              ; Jump to Modulus
 
 CMP CX, 13
-JE Rotate_Left          ; Jump to Rotate Left function
+JE Rotate_Left          ; Jump to Rotate Left
 
 CMP CX, 14
-JE Rotate_Right         ; Jump to Rotate Right function
+JE Rotate_Right         ; Jump to Rotate Right
 
-; Addition Function
-Addition:
-print 'Addition:'
+; Define functions for each operation (similar to your existing code)
+
+; Rotate Left Operation
+Rotate_Left:
+print '     ****--Rotate Left--****'
 printn
-print 'Enter first number: '
-CALL scan_num       ; Read the first number
-MOV val1, CX        ; Store it in val1   
+printn
+print 'Enter Number: '
+CALL scan_num       ; Read the input number
+MOV val1, CX        ; Store in val1   
 
-print 'Enter second number: '
-CALL scan_num       ; Read the second number
-MOV val2, CX        ; Store it in val2
-
+printn
 MOV AX, val1
-ADD AX, val2
-MOV res, AX
+ROL AX, 1           ; Rotate AX left by 1 bit
+MOV res, AX         ; Store the result in res
 
-MOV AH, 9
-MOV DX, OFFSET msg_A
+MOV AH,9
+MOV DX, OFFSET msg_RL
 INT 21h              ; Display the result message
 
 MOV AX, res
 CALL print_num       ; Print the result
 
-JMP Con               ; Go to the continuation function
+JMP Con              ; Continue flow
 
-; Subtraction Function
-Subtraction:
-print 'Subtraction:'
+; Rotate Right Operation
+Rotate_Right:
+print '     ****--Rotate Right--****'
 printn
-print 'Enter first number: '
-CALL scan_num       ; Read the first number
-MOV val1, CX        ; Store it in val1   
+printn
+print 'Enter Number: '
+CALL scan_num       ; Read the input number
+MOV val1, CX        ; Store in val1   
 
-print 'Enter second number: '
-CALL scan_num       ; Read the second number
-MOV val2, CX        ; Store it in val2
-
+printn
 MOV AX, val1
-SUB AX, val2        ; Subtraction operation
-MOV res, AX         ; Store result in `res`
+ROR AX, 1           ; Rotate AX right by 1 bit
+MOV res, AX         ; Store the result in res
 
-MOV AH, 9
-MOV DX, OFFSET msg_S
+MOV AH,9
+MOV DX, OFFSET msg_RR
 INT 21h              ; Display the result message
 
 MOV AX, res
 CALL print_num       ; Print the result
 
-JMP Con               ; Go to the continuation function
+JMP Con              ; Continue flow
 
-; Continue Function
-Con:
-printn
-MOV AH, 9
-MOV DX, OFFSET cont    ; Display continuation message
-INT 21h
-CALL scan_num          ; Read user's response
+; Rest of your code...
+
+Con:           ; Continue Function
+MOV AH,9
+MOV DX, OFFSET cont    ; Display the continuation prompt
+INT 21h                 ; Call interrupt
+print '(Yes = 1 / No = 0) : '
+CALL scan_num          ; Read the response
 MOV agn, CX
 printn
 
 CMP agn, 1
-JE Start               ; If user wants to continue, go back to Start
+JE Start               ; Jump to start if user wants to continue
 
 CMP agn, 0
-JE _Bye                ; If user wants to exit, go to the exit function
+JE _Bye                ; Jump to exit
 
-_Bye:                   ; Exit function
-MOV AH, 9
-MOV DX, OFFSET bye    ; Display the exit message
-INT 21h                ; Call interrupt to display the exit message
+Bye:                    ; Exit function
+MOV AH,9
+MOV DX, OFFSET bye     ; Display exit message
+INT 21h                ; Call interrupt
 
 MAIN ENDP
 
@@ -177,3 +177,4 @@ END MAIN
 
 HLT         ; Halting
 ret         ; Return
+
